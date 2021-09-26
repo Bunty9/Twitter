15acd0dacde7d8b2e5af2f271de52d916223f6f0
Bunty9 <cbipin2000@gmail.com>
2021-09-27 02:44:09 +0530

    final changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,32 +6,32 @@ import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { Button } from "@material-ui/core";
 
 function Sidebar() {
-  return (
-    <div className="sidebar">
-      <TwitterIcon className="sidebar__twitterIcon" />
+    return (
+        <div className="sidebar">
+            <TwitterIcon className="sidebar__twitterIcon" />
 
-      <SidebarOption active Icon={HomeIcon} text="Home" />
-      <SidebarOption Icon={SearchIcon} text="Explore" />
-      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
-      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
-      <SidebarOption Icon={ListAltIcon} text="Lists" />
-      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
-      <SidebarOption Icon={MoreHorizIcon} text="More" />
+            <SidebarOption active Icon={HomeIcon} text="Home" />
+            <SidebarOption Icon={SearchIcon} text="Explore" />
+            <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+            <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+            <SidebarOption Icon={ListAltIcon} text="Lists" />
+            <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+            <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-      {/* Button -> Tweet */}
-      <Button variant="outlined" className="sidebar__tweet" fullWidth>
-        Tweet
-      </Button>
-    </div>
-  );
+            {/* Button -> Tweet */}
+            <Button variant="outlined" className="sidebar__tweet" fullWidth>
+                Tweet
+            </Button>
+        </div>
+    );
 }
 
 export default Sidebar;
